653d1625d98d3d419059053e4141ef9c9d778802
Thejas N <thejasn.416@gmail.com>
2018-02-10 09:30:22 +0530

    Final Server

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
-# http://flask.pocoo.org/docs/patterns/fileuploads/
 import os
-from flask import Flask, request, redirect, url_for, send_from_directory
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template
 from werkzeug import secure_filename
+from collections import OrderedDict
+from operator import itemgetter
 
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+my_result={}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -13,31 +16,28 @@ def allowed_file(filename):
   # this has changed from the original example because the original did not work for me
     return filename[-3:].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            print ('**found file', file.filename)
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # for browser, add 'redirect' function on top of 'url_for'
-            return url_for('uploaded_file',
-                                    filename=filename)
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+@app.route("/")
+def main():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0',debug=True,port=5000)
+@app.route("/upload")
+def upload():
+	return render_template('upload.html')
+
+@app.route("/uploader", methods=['POST','GET'])
+def uploader():
+	if request.method == 'POST':
+		f = request.files['file']
+		f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
+		os.system("python label_image.py "+app.config['UPLOAD_FOLDER']+"/"+secure_filename(f.filename))
+		fin = open("results.txt","r")
+		my_result = eval(fin.read())
+		fin.close()
+		d = OrderedDict(sorted(my_result.items(), key=itemgetter(0),reverse=True))
+		d = list(d.items())
+	return str("Predicted Item : "+d[0][0]+" Probability :  "+str(d[0][1]))
+
+
+if __name__ == "__main__":
+    app.run()
